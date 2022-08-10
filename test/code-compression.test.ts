0fa2e3a281d9e68beac151bbab103422b220011c
K <1175047471@qq.com>
2022-08-10 14:01:10 +0800

    chore

--- test/code-compression.test.ts
+++ test/code-compression.test.ts
@@ -10,9 +10,25 @@ it('test', () => {
   const { code } = transformFileSync(join(__dirname, '../mock/code.js'), {
     plugins: [codeCompression],
     generatorOpts: {
-      compact: true, // 去除空格
+      // compact: true, // 去除空格
       comments: false // 移除注释
     }
   })
-  expect(code).toMatchInlineSnapshot('"function _g(){const _c=1;const _d=2;_f(3,4);console.log(_d);return _d;function _f(_a,_b){return _a+_b;}}_g();"')
+  expect(code).toMatchInlineSnapshot(`
+    "function _g() {
+      const _c = 1;
+      const _d = 2;
+
+      _f(3, 4);
+
+      console.log(_d);
+      return _d;
+
+      function _f(_a, _b) {
+        return _a + _b;
+      }
+    }
+
+    _g();"
+  `)
 })
